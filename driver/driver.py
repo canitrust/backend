168199c0366af33eae04380aa028010f6a1c190c
khanhhongpham <khanh.hong.pham@mgm-tp.com>
2019-11-06 18:32:30 +0700

    Test Pull Request

--- driver/driver.py
+++ driver/driver.py
@@ -93,6 +93,7 @@ def run_test_local(test_case):
 
 def run_local_main():
     logger.info('Environment: Local')
+    logger.info('Test Pull Request')
     local_tests = []
     if TESTCASES:
         logger.info('List test cases:{}'.format(TESTCASES))
