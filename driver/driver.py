7da9c33763b69e48a66639503ff02114b6a3092a
khanhhongpham <khanh.hong.pham@mgm-tp.com>
2019-11-07 08:53:08 +0700

    Trying

--- driver/driver.py
+++ driver/driver.py
@@ -96,7 +96,7 @@ def run_test_local(test_case):
 
 def run_local_main():
     logger.info('Environment: Local')
-    logger.info('Test Pull request with github actions')
+    logger.info('Test Pull request with github actions 2')
     local_tests = []
     if TESTCASES:
         logger.info('List test cases:{}'.format(TESTCASES))
