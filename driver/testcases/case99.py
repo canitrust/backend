8f2ca7c0fd956432773df9b7ccc01c3c3d32a9ce
Benjamin Daniel Mussler <10850706+bdmussler@users.noreply.github.com>
2019-12-25 20:54:55 +0100

    Determine whether browsers consider "application/json" and/or "text/json" as valid Content-Types for scripts.
    ==============================
    
    Firefox 50:
    
    https://blog.mozilla.org/security/2016/08/26/mitigating-mime-confusion-attacks-in-firefox/
    
    [...]
    
    Valid Content-Types for Scripts:
    – “application/javascript”
    – “application/x-javascript”
    – “application/ecmascript”
    – “application/json”<---
    – “text/ecmascript”
    – “text/javascript”
    – “text/json”   <---
    
    ==============================
    
    Firefox 60:
    
    https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Releases/60
    
    Security
    
    The X-Content-Type-Options header, when set to no-sniff, now follows
    the specification for JavaScript MIME types. In particular,
    text/json and application/json are no longer valid values (bug 1431095).
    
    ==============================

---
+++ driver/testcases/case99.py
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------------------------------------
+#  Copyright (c) mgm security partners GmbH. All rights reserved.
+#  Licensed under the AGPLv3 License. See LICENSE.md in the project root for license information.
+#-------------------------------------------------------------------------------------------------
+
+from testcases.testCase import TestCase
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from helper import Logger
+logger = Logger(__name__).logger
+
+class Case99(TestCase):
+
+    def __init__(self):
+        TestCase.__init__(self)
+        self.testCaseNum = 99
+
+    def executeTest(self, webDriver):
+        """ Definition of a testcase
+            Test result MUST be set to self.data
+        """
+        webDriver.get("http://jsonpinc.test-canitrust.com/case99/index.html")
+        WebDriverWait(webDriver, 10).until(EC.title_contains(('finished')))
+        div_result = webDriver.find_element_by_tag_name("div").text
+
+        webDriver.close()
+
+        if (div_result == 'div_result'):
+            self.data = {'div_result': '-99'}
+        else:
+            self.data = {'div_result': div_result}
+
+        return 1
+
+    def evaluate(self):
+	# Possible results:
+	# -99 = driver issue
+	# -1  = JS/page issue
+	#  0  = test succeeded, nothing executed
+	# >0  = test succeeded, something executed ("something" defined by value)
+        self.result = self.data['div_result']
