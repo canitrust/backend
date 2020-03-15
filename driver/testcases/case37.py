9119a5e07589d9bc78ddc9a3fe864571ec13e02f
local_mlnguyen <local_mlnguyen@MINHLNB.mgm-edv.de>
2020-03-15 11:02:37 +0700

    Fix testcase

--- driver/testcases/case37.py
+++ driver/testcases/case37.py
@@ -21,14 +21,30 @@ class Case37(TestCase):
         webDriver.get("http://csp6.test-canitrust.com/contradict-csp-header-and-meta2.html")
         WebDriverWait(webDriver, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'script')))
         content2 = webDriver.find_element_by_id('content2').text
+        webDriver.get("http://csp7.test-canitrust.com/contradict-csp-header-and-meta3.html")
+        WebDriverWait(webDriver, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'script')))
+        content3 = webDriver.find_element_by_id('content3').text
+        webDriver.get("http://csp8.test-canitrust.com/contradict-csp-header-and-meta4.html")
+        WebDriverWait(webDriver, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'script')))
+        content4 = webDriver.find_element_by_id('content4').text
+        webDriver.get("http://csp9.test-canitrust.com/contradict-csp-header-and-meta5.html")
+        WebDriverWait(webDriver, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'script')))
+        content5 = webDriver.find_element_by_id('content5').text
+        webDriver.get("http://csp10.test-canitrust.com/contradict-csp-header-and-meta6.html")
+        WebDriverWait(webDriver, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'script')))
+        content6 = webDriver.find_element_by_id('content6').text
         webDriver.close()
 
-        self.data = { 'content1': content1, 'content2': content2 }
+        self.data = { 'Header page 1': content1,
+                      'Meta page 1': content2,
+                      'Header page 2': content3,
+                      'Meta page 2': content4,
+                      'Is Stricter page 1': content5,
+                      'Is Stricter page 2': content6 }
         return 1
 
     def evaluate(self):
-        if self.data['content1'] == 'Content1':
-          if self.data['content2'] == 'Content2':
+        if self.data:
             result = 2
         else:
           result = 0
